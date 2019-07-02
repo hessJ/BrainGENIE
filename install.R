6cc2a9fce13b5cc612b8577e84e80946a6b9b8c6
Jon Hess <jonathanhess@Jonathans-MacBook-Pro-2.local>
2019-07-02 11:32:12 -0400

    rtracklayer

--- install.R
+++ install.R
@@ -5,6 +5,7 @@ install.packages("glmnet")
 install.packages("WGCNA")
 install.packages("reshape2")
 install.packages("broom")
+install.packages("rtracklayer")
 
 # -- bioconductor packages
 BiocManager::install("edgeR")
