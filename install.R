b7f2ab64d29f0dce306668433432d2b62b5ca0a7
Jon Hess <jonathanhess@Jonathans-MacBook-Pro-2.local>
2019-07-02 11:31:12 -0400

    Install script

--- install.R
+++ install.R
@@ -1,1 +1,11 @@
-# TODO: Install CRAN/Bioconductor dependencies for BrainGENIE
+# -- CRAN packages
+install.packages("data.table")
+install.packages("plyr")
+install.packages("glmnet")
+install.packages("WGCNA")
+install.packages("reshape2")
+install.packages("broom")
+
+# -- bioconductor packages
+BiocManager::install("edgeR")
+BiocManager::install("limma")
